9201652b490b7c38265a152212209ed78ee88d52
An3Dev <an3Developer@gmail.com>
2018-03-10 12:56:43 -0800

    Working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float lookSensitivity = 5f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Calculate movement velocity as a 3D vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float yMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * yMov;
+
+        // Final movement vector;
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+
+        // Apply movement
+        motor.Move(velocity);
+
+
+        // Calculate rotation as a 3D vector3 (turning around)
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0, yRot, 0) * lookSensitivity;
+
+        // Apply rotation
+        motor.Rotate(rotation);
+
+        // Calculate camera rotation as a 3D vector3 (turning around)
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 cameraRotation = new Vector3(xRot, 0, 0) * lookSensitivity;
+
+        // Apply camera rotation
+        motor.RotateCamera(cameraRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerShoot : NetworkBehaviour
+{
+
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit _hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+        {
+            Debug.Log(_hit.collider.name + " was hit");
+        }
+
+    }
+
+}
